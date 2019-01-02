94e2c2dee7b37fbdc902e03000b058b877a885a1
Sukma Raharja <sukmaraharja@outlook.co.id>
2019-01-02 21:29:46 +0800

    add todo app

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-alert('hello')
+function addItem() {
+    var textInput = document.getElementById('text-input')
+    var text = textInput.value
+    text = text.trim()
+
+    if(text == ''){
+        alert('Field empty!')
+        textInput.value = ''
+        return
+    }
+
+    var newItem = document.createElement('li')
+    newItem.innerHTML = text
+
+    var list = document.getElementById('list')
+    list.insertBefore(newItem, list.childNodes[0])
+    textInput.value = ''
+
+    newItem.onclick = function () { this.parentNode.removeChild(this) }
+}
