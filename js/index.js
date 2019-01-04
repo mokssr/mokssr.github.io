af57d4c5dd4f6a488ec2f0bb949291a2ed25b7de
Sukma Raharja <sukmaraharja@outlook.co.id>
2019-01-04 19:58:16 +0800

    add push notification

--- js/index.js
+++ js/index.js
@@ -1,19 +1,53 @@
-if('serviceWorker' in navigator){
-    window.addEventListener('load', ()=>{
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
         navigator.serviceWorker
             .register('./../service-worker.js')
-            .then(reg=> console.log('Service worker registered'))
-            .catch(err=> console.log(`error ${err}`))
+            .then(reg => console.log('Service worker registered'))
+            .catch(err => console.log(`error ${err}`))
     })
 }
 
+if (!('Notification' in window)) {
+    console.log('Browser dont support notification')
+}
+Notification.requestPermission(function (status) {
+    console.log('Notification status : ' + status)
+})
+
+function showNotification() {
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(function (reg) {
+            var options = {
+                body: 'Task Deleted!',
+                icon: 'images/notification-flat.png',
+                vibrate: [100, 50, 100],
+                data: {
+                    dateOfArrival: Date.now(),
+                    primaryKey: 1
+                },
+                actions: [
+                    {
+                        action: 'explore', title: 'Go to the site',
+                        // icon: 'images/checkmark.png'
+                    },
+                    {
+                        action: 'close', title: 'Close the notification',
+                        // icon: 'images/xmark.png'
+                    },
+                ]
+            };
+
+            reg.showNotification('ToDo Application', options)
+        })
+    }
+}
 
 function addItem() {
     var textInput = document.getElementById('text-input')
     var text = textInput.value
     text = text.trim()
 
-    if(text == ''){
+    if (text == '') {
         alert('Field empty!')
         textInput.value = ''
         textInput.focus()
@@ -27,5 +61,5 @@ function addItem() {
     list.insertBefore(newItem, list.childNodes[0])
     textInput.value = ''
 
-    newItem.onclick = function () { this.parentNode.removeChild(this) }
+    newItem.onclick = function () { this.parentNode.removeChild(this); showNotification() }
 }
